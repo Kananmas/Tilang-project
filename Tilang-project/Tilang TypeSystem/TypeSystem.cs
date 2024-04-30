98863afc2be944a4752a557c083cfc829c0ed611
Kanan masajedi <Kananmasajedi@gmail.com>
2024-04-30 12:27:57 +0330

    done

---
+++ Tilang-project/Tilang TypeSystem/TypeSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilang_project.Tilang_TypeSystem
+{
+    public class TypeSystem
+    {
+
+    }
+}
--- Tilang-project/Tilang tokenizer/Tokenizer.cs
+++ Tilang-project/Tilang tokenizer/Tokenizer.cs
@@ -12,12 +12,80 @@ namespace Tilang_project.Tilang_tokenizer
 
         public static List<string> Tokenization(string sourceCode)
         {
+            var spaceIndex = sourceCode.IndexOf(' ');
+            if (spaceIndex <= -1) return new List<string> { sourceCode };
+            var statement = sourceCode.Substring(0, spaceIndex);
+            var result = new List<string>();
 
+            result.Add(statement);
+
+            switch (statement)
+            {
+                case "function":
+                    var fnName = sourceCode.Substring(sourceCode.IndexOf(" "), sourceCode.IndexOf("(")
+                        - sourceCode.IndexOf(" ")).Trim();
+                    result.Add(fnName);
+                    var fnArgs = sourceCode.Substring(sourceCode.IndexOf("("), sourceCode.IndexOf(")")
+                        - sourceCode.IndexOf("(") + 1);
+                    result.Add(fnArgs);
+                    var fnBody = sourceCode.Substring(sourceCode.IndexOf("{"));
+                    result.Add(fnBody);
+
+                    if (sourceCode.IndexOf("<") == -1) break;
+
+                    var fnReturnType = sourceCode.Substring(sourceCode.IndexOf("<") + 1,
+                        sourceCode.IndexOf(">") - sourceCode.IndexOf("<") - 1);
+                    result.Add(fnReturnType);
+                    break;
+                case "var":
+                case "const":
+                    result = Reform(sourceCode.Split(" ").ToList());
+                    break;
+                case "type":
+                    result = ConfigureCustomType(sourceCode);
+                    break;
+                default:
+                    result.Clear();
+                    result.Add(sourceCode);
+                    break;
+            }
+            return result;
         }
 
 
 
-        private static string[] lineSeperator(string sourceCode)
+        private static List<string> Reform(List<string> subTokens)
+        {
+            var result = new List<string>();
+            var indexOfEqual = subTokens.IndexOf("=");
+            string value = "";
+            if (indexOfEqual != -1)
+            {
+                for(int i = 0 ; i < subTokens.Count; i++)
+                {
+                    if(i < indexOfEqual)
+                    {
+                        result.Add(subTokens[i]);
+                    }
+                    else
+                    {
+                        value += subTokens[i];
+                    }
+                }
+
+                if(value.Length > 0)
+                {
+                    result.Add(value);
+                }
+            }
+
+            
+            return subTokens;
+        }
+
+
+
+        private static string[] sourceCodeSeperator(string sourceCode)
         {
             sourceCode = codeReformer(sourceCode);
             var result = new List<string>();
