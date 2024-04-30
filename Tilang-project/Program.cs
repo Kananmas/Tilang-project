c15245518f14ae42f233d2f3252c5e8853c5f739
Kanan masajedi <Kananmasajedi@gmail.com>
2024-04-30 12:10:41 +0330

    base

--- Tilang-project/Program.cs
+++ Tilang-project/Program.cs
@@ -1,18 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more informatiov
 
-using Tilang_project.LexicalTree;
-using Tilang_project.Parser;
-
-Parser parser = new Parser();
-LexicalTree lexicalTree = new LexicalTree();
 
 var codeFile = File.ReadAllText("C:\\Users\\ASUS\\Desktop\\projects\\Tilang-project\\Tilang-project\\code-sample.txt");
 
-
-var lines = parser.LineSeparator(codeFile);
-var tokenization = parser.LineByKeywords(lines);
-
-
-var scope  = lexicalTree.GenerateScope(tokenization);
 
 Console.WriteLine();
---
+++ Tilang-project/Tilang tokenizer/Tokenizer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilang_project.Tilang_tokenizer
+{
+    public static class Tokenizer
+    {
+        private static string[] lineSeperator(string sourceCode)
+        {
+            var reformedString = codeReformer(sourceCode);
+            var line = "";
+
+            for(int i = 0; i < reformedString.Length; i++)
+            {
+                var character = reformedString[i];
+                
+                if(character == ';')
+                {
+                    
+                }
+
+            }
+        }
+
+        private static string codeReformer(string sourceCode)
+        {
+            return sourceCode.Replace("<" , " <").Replace("=" , " = ").Replace("{" , " {").Replace("}","};");
+        }
+    }
+}
