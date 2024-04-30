0f8ce07dc3e2d254a388f40d31639576236c4382
Kanan masajedi <Kananmasajedi@gmail.com>
2024-04-30 12:17:11 +0330

    ok

--- Tilang-project/Tilang tokenizer/Tokenizer.cs
+++ Tilang-project/Tilang tokenizer/Tokenizer.cs
@@ -9,26 +9,88 @@ namespace Tilang_project.Tilang_tokenizer
 {
     public static class Tokenizer
     {
+
+        public static List<string> Tokenization(string sourceCode)
+        {
+
+        }
+
+
+
         private static string[] lineSeperator(string sourceCode)
         {
-            var reformedString = codeReformer(sourceCode);
-            var line = "";
+            sourceCode = codeReformer(sourceCode);
+            var result = new List<string>();
+            var currentVal = "";
 
-            for(int i = 0; i < reformedString.Length; i++)
+            var isInPrantesis = false;
+            var prCount = 0;
+            var isInCurvyBrackeys = false;
+            var brackeysCount = 0;
+
+            for (int i = 0; i < sourceCode.Length; i++)
             {
-                var character = reformedString[i];
-                
-                if(character == ';')
+                var currentChar = sourceCode[i];
+
+                if (currentChar == '(')
                 {
-                    
+                    if (!isInPrantesis) isInPrantesis = true;
+                    prCount++;
+                }
+                if (currentChar == '{')
+                {
+                    if (!isInCurvyBrackeys) isInCurvyBrackeys = true;
+                    brackeysCount++;
+                }
+                if (currentChar != ';')
+                {
+                    currentVal += currentChar;
                 }
 
+                else
+                {
+                    if (!isInPrantesis && !isInCurvyBrackeys)
+                    {
+                        if (currentVal.Length > 0) result.Add(currentVal.Trim());
+                        currentVal = "";
+                    }
+                    else
+                    {
+                        currentVal += currentChar;
+                    }
+                }
+
+
+
+                if (currentChar == ')')
+                {
+                    prCount--;
+                    if (prCount == 0)
+                    {
+                        isInPrantesis = false;
+                    }
+                }
+                if (currentChar == '}')
+                {
+                    brackeysCount--;
+                    if (brackeysCount == 0)
+                    {
+                        isInCurvyBrackeys = false;
+                    }
+                }
             }
+            if (currentVal.Length > 0)
+            {
+                result.Add(currentVal);
+            }
+
+
+            return result.ToArray();
         }
 
         private static string codeReformer(string sourceCode)
         {
-            return sourceCode.Replace("<" , " <").Replace("=" , " = ").Replace("{" , " {").Replace("}","};");
+            return sourceCode.Replace("<" , " <").Replace("=" , " = ").Replace("{" , " {").Replace("}"," };");
         }
     }
 }
